eebec83ad5a0bd4dd091dfca836239453fe17c65
Antoine HAMON <antoine.hamon22@gmail.com>
2019-09-09 11:35:03 +0000

    whole

--- hello/hello.go
+++ hello/hello.go
@@ -5,7 +5,29 @@ import (
 	"fmt"
 	//"strconv"
 	// "time"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
 )
+
+// go get -u github.com/faiface/pixel/pixel
+// go get -u github.com/faiface/pixel/pixelgl
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Test",
+		Bounds: pixel.R(0, 0, 160*3, 144*3),
+		VSync:  false,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for !win.Closed() {
+		}
+	}()
+}
 
 func main() {
 	core := new(core.GbCore)
@@ -31,5 +53,10 @@ func main() {
 	
 	fmt.Println(core.GbPpu.Buffer)
 
+	
+	pixelgl.Run(func() {
+		run()
+	})
+	
 	p("|================ END ===============|")
 }
